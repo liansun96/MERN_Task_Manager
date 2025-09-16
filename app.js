82250bf630ad99d54553d9d03bdc7607dc5f8646
liansun96 <liansun0203@gmail.com>
2025-09-16 23:41:45 +0630

    categorize folder

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express') 
+const app = express()
+
+const port = process.env.PORT || 3000
+
+const start = async() => {
+    app.listen(port , console.log(`Server is listen on ${port}`))
+}
+
+start()
